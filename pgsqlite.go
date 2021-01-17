b8c7003b65e8b8f187368bd9b1d370d0bf0de2e5
Jim Kalafut <jim@kalafut.net>
2021-01-16 17:31:04 -0800

    Cleanup

--- pgsqlite.go
+++ pgsqlite.go
@@ -3,28 +3,28 @@ package pgsqlite
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"regexp"
-	"strings"
-
-	"github.com/kalafut/q"
 )
+
+var paramRe = regexp.MustCompile(`\$\d+`)
+var returnRe = regexp.MustCompile(`(?i)\s*RETURNING\s+\w+;?\s*$`)
 
 type MyDriver struct {
 	*sql.DB
-	//sqlite bool
 }
 
-type result struct {
-	id int64
+func Open(driverName, dataSourceName string) (*MyDriver, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return New(db), nil
 }
 
-func (r *result) LastInsertId() (int64, error) {
-	return r.id, nil
-}
-
-func (r *result) RowsAffected() (int64, error) {
-	return 0, errors.New("RowsAffected is not supported by this wrapper")
+func New(db *sql.DB) *MyDriver {
+	return &MyDriver{
+		DB: db,
+	}
 }
 
 func (db *MyDriver) QueryRow(query string, args ...interface{}) *sql.Row {
@@ -32,128 +32,28 @@ func (db *MyDriver) QueryRow(query string, args ...interface{}) *sql.Row {
 }
 
 func (db *MyDriver) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
-	query = strings.TrimSpace(query)
+	query, returnStmt := rewrite(query)
 
-	re2 := regexp.MustCompile(`RETURNING\s+\w+`)
-
-	fetchID := re2.FindString(query) != ""
-
-	query = re2.ReplaceAllString(query, "")
-	query = rebind(query)
-
-	q.Q(query)
-	if fetchID {
-		q.Q("here")
+	if returnStmt {
 		db.DB.ExecContext(ctx, query, args...)
-		//if row.Err() != nil {
-		//	return row
-		//}
 
 		return db.DB.QueryRow("SELECT last_insert_rowid();")
 	}
 
-	//q.Q(query)
-	//r, err := db.DB.ExecContext(ctx, query, args...)
-	//id, err := r.LastInsertId()
-	//q.Q(id, err)
-
-	//row := db.DB.QueryRowContext(ctx, query, args...)
-	//q.Q(row.)
-
 	return db.DB.QueryRowContext(ctx, query, args...)
 }
 
-func rewrite(query string) string {
-	re2 := regexp.MustCompile(`(?i)\s*RETURNING\s+\w+;?\s*$`)
-	q.Q(re2.FindString(query))
+func rewrite(query string) (string, bool) {
+	// replace RETURN statements
+	queryNew := returnRe.ReplaceAllString(query, "")
 
-	query = re2.ReplaceAllString(query, "")
-	query = rebind(query)
+	returnStmt := (queryNew != query)
 
-	return query
-}
-
-//func (db *MyDriver) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
-//	var err error
-//	query = strings.TrimSpace(query)
-//
-//	re := regexp.MustCompile(`^INSERT.*RETURNING\s+\w+`)
-//	re2 := regexp.MustCompile(`RETURNING\s+\w+`)
-//
-//	if db.sqlite {
-//		query = re2.ReplaceAllString(query, "")
-//		query, err = normalizeQuery(query)
-//		if err != nil {
-//			return nil, err
-//		}
-//	}
-//
-//	if re.MatchString(query) {
-//		r := &result{}
-//		rows := db.QueryRowContext(ctx, query, args...)
-//		err := rows.Scan(&r.id)
-//
-//		return r, err
-//	}
-//
-//	return db.DB.ExecContext(ctx, query, args...)
-//}
-
-func Open(driverName, dataSourceName string) (*MyDriver, error) {
-	db, err := sql.Open(driverName, dataSourceName)
-	if err != nil {
-		return nil, err
-	}
-	return &MyDriver{
-		DB: db,
-		//sqlite: strings.TrimSpace(driverName) == "sqlite3",
-	}, nil
-}
-
-var paramRe = regexp.MustCompile(`\$\d+`)
-
-// rebind will replace postgres $1, $2, ... parameters with
-// with sqlite's ?1, ?2 style.
-func rebind(s string) string {
-	s = paramRe.ReplaceAllStringFunc(s, func(m string) string {
+	// replace postgres-sql $1, $2, ... parameters with
+	// with sqlite's ?1, ?2 style.
+	queryNew = paramRe.ReplaceAllStringFunc(queryNew, func(m string) string {
 		return "?" + m[1:]
 	})
-	return s
-}
 
-//func main() {
-//	d, err := Open("pgx", `postgres://kalafut@localhost/snipstream?sslmode=disable`)
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	result, err := d.Exec("INSERT INTO snippets(text) VALUES($1) RETURNING id", "abc")
-//	//result, err := d.Exec("INSERT INTO snippets(text) VALUES($1)", "abc")
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	fmt.Println(result.RowsAffected())
-//	fmt.Println(result.LastInsertId())
-//
-//	db, err := Open("sqlite3", "./foo.db")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	result, err = db.Exec("INSERT INTO snippets(text) VALUES($1) RETURNING id", "abc")
-//	//result, err := d.Exec("INSERT INTO snippets(text) VALUES($1)", "abc")
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	fmt.Println(result.RowsAffected())
-//	fmt.Println(result.LastInsertId())
-//
-//	_, err = d.Query("SELECT * from snippets")
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//}
-//
+	return queryNew, returnStmt
+}
--- pgsqlite_test.go
+++ pgsqlite_test.go
@@ -1,7 +1,9 @@
 package pgsqlite
 
 import (
+	"database/sql"
 	"os"
+	"path/filepath"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -11,19 +13,16 @@ import (
 func TestSQLite(t *testing.T) {
 	assert := assert.New(t)
 
-	//dbfile := filepath.Join(os.TempDir(), "test.db")
-	dbfile := "./test.db"
+	dbfile := filepath.Join(os.TempDir(), "test.db")
 	os.Remove(dbfile)
-	db, err := Open("sqlite3", dbfile)
-	if err != nil {
-		panic(err)
-	}
-	//defer os.Remove(dbfile)
+
+	dbRaw, err := sql.Open("sqlite3", dbfile)
+	assert.Nil(err)
+
+	db := New(dbRaw)
 
 	_, err = db.Exec(`CREATE TABLE users(id INTEGER, first TEXT, last, TEXT, age INTEGER);`)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Nil(err)
 
 	_, err = db.Exec(`INSERT INTO users(first, last) VALUES('John', 'Doe');`)
 	assert.Nil(err)
@@ -36,6 +35,12 @@ func TestSQLite(t *testing.T) {
 	assert.Nil(row.Scan(&first, &last))
 	assert.Equal("John", first)
 	assert.Equal("Doe", last)
+
+	assert.Nil(db.Close())
+
+	// Lets try Open
+	db, err = Open("sqlite3", dbfile)
+	assert.Nil(err)
 
 	row = db.QueryRow(`INSERT INTO users(first, last) VALUES('Jane', 'Smith') RETURNING id;`)
 	assert.Nil(row.Err())
@@ -51,31 +56,6 @@ func TestSQLite(t *testing.T) {
 	assert.Nil(row.Scan(&first, &last))
 	assert.Equal("Jane", first)
 	assert.Equal("Smith", last)
-}
-
-func Test_rebind(t *testing.T) {
-	tests := []struct {
-		in  string
-		out string
-	}{
-		{
-			in:  "SELECT * FROM USERS;",
-			out: "SELECT * FROM USERS;",
-		},
-		{
-			in:  "INSERT INTO USERS (a,b,c) VALUES ($1, $2, $3)",
-			out: "INSERT INTO USERS (a,b,c) VALUES (?1, ?2, ?3)",
-		},
-		{
-			in:  "INSERT INTO USERS (a,b,c) VALUES ($3, $2, $2)",
-			out: "INSERT INTO USERS (a,b,c) VALUES (?3, ?2, ?2)",
-		},
-	}
-	for _, test := range tests {
-		if act := rebind(test.in); act != test.out {
-			t.Errorf("error translating %q. Expected %q, got %q", test.in, test.out, act)
-		}
-	}
 }
 
 func Test_rewrite(t *testing.T) {
@@ -118,7 +98,7 @@ func Test_rewrite(t *testing.T) {
 		},
 	}
 	for name, test := range tests {
-		act := rewrite(test.in)
+		act, _ := rewrite(test.in)
 		if act != test.out {
 			t.Errorf("error in %q. Expected %q, got %q", name, test.out, act)
 		}
